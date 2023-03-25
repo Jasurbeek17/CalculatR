ce899de4387b4d21aa65332ec107aef5eba2418d
Jasurbeek17 <jasurismatov50@gmail.com>
2023-03-25 11:22:38 +0500

    upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//CalculatR
+// 09.03.2023
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string password = "";
+
+        do
+        {
+            Console.Write("Enter your password: ");
+            password = Console.ReadLine();
+        } while (password != "qwerty");
+
+        Console.Write("\nEnter first number: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter operation (+, -, *, /, %): ");
+        char operation = Convert.ToChar(Console.ReadLine()[0]);
+
+        Console.Write("Enter second number: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        
+    }
+}
