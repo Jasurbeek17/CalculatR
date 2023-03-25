e0a08915f475f913fc4557f57000e5db139f11f4
Jasurbeek17 <jasurismatov50@gmail.com>
2023-03-25 15:18:42 +0500

    upload 2

--- Program.cs
+++ Program.cs
@@ -23,6 +23,46 @@ internal class Program
         Console.Write("Enter second number: ");
         int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-        
+        //ternary
+        string message = 
+            !(firstNumber >= 0) 
+            ? "\n1st number is not positive!" 
+            : "\n1st number is positive!";
+        System.Console.WriteLine(message);
+
+        if (firstNumber > secondNumber)
+        {
+            System.Console.WriteLine("\n1st number is greter than 2nd number.");
+        }
+        else if (firstNumber == secondNumber)
+        {
+            System.Console.WriteLine("\n1st number is equal to 2nd number.");
+        }
+        else
+        {
+            System.Console.WriteLine("\n1st number is less than 2nd number.");
+        }
+
+        string result = operation switch
+        {
+            '+' => $"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}",
+            '-' => $"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}",
+            '*' => $"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}",
+            '/' => $"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}",
+            '%' => $"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}",
+            _ => "Operation not found!"
+        };
+        System.Console.WriteLine($"\n{result}\n");
+
+        //karra jadvali:
+        for (int i = 1; i < 10; i ++)
+        {
+            for (int j = 1; j < 10; j ++)
+            {
+                System.Console.WriteLine($"{i}x{j}={i*j}");
+            }
+            System.Console.WriteLine();
+        }
+
     }
 }
